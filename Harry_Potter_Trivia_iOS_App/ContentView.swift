b612ebb09b3a7287d415acccc5889555f9ce47df
lyleverythinG <75658617+lyleverythinG@users.noreply.github.com>
2024-12-17 12:13:05 +0800

    Finish settings view

--- Harry_Potter_Trivia_iOS_App/ContentView.swift
+++ Harry_Potter_Trivia_iOS_App/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
     @State private var moveBgImage = false
     @State private var animateViewsIn = false
     @State private var showInstructions = false
+    @State private var showSettings = false
     
     var body: some View {
         GeometryReader { geo in
@@ -131,7 +132,7 @@ struct ContentView: View {
                         VStack {
                             if animateViewsIn {
                                 Button {
-                                    //Show settings screen
+                                    showSettings.toggle()
                                 } label: {
                                     Image(systemName: "gearshape.fill")
                                         .font(.largeTitle)
@@ -139,6 +140,9 @@ struct ContentView: View {
                                         .shadow(radius: 5)
                                 }
                                 .transition(.offset(x: geo.size.width / 4))
+                                .sheet(isPresented: $showSettings) {
+                                    Settings()
+                                }
                             }
                         }
                         .animation(.easeOut(duration: 0.7).delay(2.7),
